ecf3ccbe52249aef9e65fef796b39e328fb8fa9c
Tito Nobre <tito.nobre@blip.pt>
2023-02-07 01:43:59 +0000

    add basic todo app logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import { useCallback, useState } from "react";
+
+import styles from "./App.module.css";
+
 function App() {
-  return <div></div>;
+  const [items, setItems] = useState<string[]>([]);
+
+  const onRemove = useCallback(
+    (itemToRemove: string) => {
+      setItems(items.filter((item) => item != itemToRemove));
+    },
+    [items]
+  );
+
+  const onAdd = useCallback(
+    (itemToAdd: string) => {
+      setItems([...items, itemToAdd]);
+    },
+    [items]
+  );
+
+  return (
+    <div className={styles.app}>
+      <ul>
+        {items.map((item) => {
+          return (
+            <li key={item}>
+              {item} <button onClick={() => onRemove(item)}>X</button>
+            </li>
+          );
+        })}
+      </ul>
+
+      <button onClick={() => onAdd(`todo ${items.length}`)}>+</button>
+    </div>
+  );
 }
 
 export default App;
