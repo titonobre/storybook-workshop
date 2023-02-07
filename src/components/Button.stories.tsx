ae8fd230d9db829f2c2634dff3dab0c8d886ba21
Tito Nobre <tito.nobre@blip.pt>
2023-02-07 01:16:32 +0000

    add button component

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+export default {
+  component: Button,
+  tags: ["autodocs"],
+} as Meta<typeof Button>;
+
+export const Default: StoryObj<typeof Button> = {
+  args: {
+    label: "Button",
+  },
+};
+
+export const Primary: StoryObj<typeof Button> = {
+  args: {
+    theme: "primary",
+    label: "Button",
+  },
+};
+
+export const Secondary: StoryObj<typeof Button> = {
+  args: {
+    theme: "secondary",
+    label: "Button",
+  },
+};
