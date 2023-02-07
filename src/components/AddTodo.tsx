e266ced45885228a1ab4f3cdfea5c17a5bcd21b5
Tito Nobre <tito.nobre@blip.pt>
2023-02-07 01:20:43 +0000

    add add todo component

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+
+import { Button } from "./Button";
+
+import styles from "./AddTodo.module.css";
+
+export type AddTodoProps = {
+  onAddTodo: (text: string) => void;
+};
+
+export const AddTodo = ({ onAddTodo }: AddTodoProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onClickAddButton = () => {
+    if (inputRef.current) {
+      const value = inputRef.current.value;
+      value && onAddTodo?.(value);
+    }
+  };
+
+  return (
+    <div className={styles.addTodo}>
+      <input ref={inputRef}  className={styles.addTodoInput} type={"text"} />
+      <Button theme="primary" label="+" onClick={onClickAddButton} />
+    </div>
+  );
+};
